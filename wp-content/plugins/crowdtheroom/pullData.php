339b2b6fb6f399b099bbe7b0e64ec6c8cc2c93a8
dstroik24 <danielstroik@utexas.edu>
2018-11-14 21:13:34 -0600

    indent

--- wp-content/plugins/crowdtheroom/pullData.php
+++ wp-content/plugins/crowdtheroom/pullData.php
@@ -2,12 +2,13 @@
   function get_ctr_users(){
     global $wpdb;
     $table_name = $wpdb->prefix . 'ctr_users';
-      $sql = "SELECT * FROM " . $table_name;
+    $sql = "SELECT * FROM " . $table_name;
     echo $sql;
     $result = $wpdb->get_results($sql);
     foreach($result as $print) {
       echo $print->user_fname, '<br>';
     }
   }  
+  
   get_ctr_users();
 ?>
