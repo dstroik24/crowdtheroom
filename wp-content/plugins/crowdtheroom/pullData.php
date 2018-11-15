d45ac6953b5c7941497df6cc5ea6968f980f3929
dstroik24 <danielstroik@utexas.edu>
2018-11-14 20:38:25 -0600

    why is this not working

--- wp-content/plugins/crowdtheroom/pullData.php
+++ wp-content/plugins/crowdtheroom/pullData.php
@@ -4,6 +4,8 @@
   $result = $wpdb->get_results ( "SELECT * FROM wordpress.wp_ctr_users" );
   echo "testing";
   foreach ( $result as $print )   {
-    echo $print;
+    ?>
+    <p>Result 1 $print</p>
+    <?php
   }
 ?>
