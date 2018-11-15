a05c5316bf213716e1acf2a10cc10681d6750681
dstroik24 <danielstroik@utexas.edu>
2018-11-14 20:33:56 -0600

    change pulllData

--- wp-content/plugins/crowdtheroom/pullData.php
+++ wp-content/plugins/crowdtheroom/pullData.php
@@ -3,14 +3,12 @@
  <th>user_fname</th>
  <th>user_lname</th>
 </tr>
-  <?php
-    global $wpdb;
-    $result = $wpdb->get_results ( "SELECT * FROM wordpress.wp_ctr_users" );
-    echo "testing";
-    foreach ( $result as $print )   {
-    ?>
-    <tr>
-    <td><?php echo $print->user_fname;?></td>
-    </tr>
-        <?php }
-  ?>
+  
+<?php
+  global $wpdb;
+  $result = $wpdb->get_results ( "SELECT * FROM wordpress.wp_ctr_users" );
+  echo "testing";
+  foreach ( $result as $print )   {
+    echo $print;
+  }
+?>
