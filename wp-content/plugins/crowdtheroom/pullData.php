0ebd7a83118b497e5f9365d07850e3c2e9ddba41
dstroik24 <danielstroik@utexas.edu>
2018-11-14 20:32:13 -0600

    change again

--- wp-content/plugins/crowdtheroom/pullData.php
+++ wp-content/plugins/crowdtheroom/pullData.php
@@ -6,6 +6,7 @@
   <?php
     global $wpdb;
     $result = $wpdb->get_results ( "SELECT * FROM wordpress.wp_ctr_users" );
+    echo "testing";
     foreach ( $result as $print )   {
     ?>
     <tr>
