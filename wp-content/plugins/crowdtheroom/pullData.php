3c681a5bbe443fdd21cfce95106dee78608725a7
dstroik24 <danielstroik@utexas.edu>
2018-11-14 21:09:33 -0600

    include in other file

--- wp-content/plugins/crowdtheroom/pullData.php
+++ wp-content/plugins/crowdtheroom/pullData.php
@@ -1,11 +1,5 @@
 
 <?php
-  global $wpdb;
-  $result = $wpdb->get_results ( "SELECT * FROM wordpress.wp_ctr_users" );
-  echo "testing";
-  foreach ( $result as $print )   {
-    ?>
-    <p>Result 1 $print</p>
-    <?php
-  }
+  include 'crowdtheroom.php';
+  get_ctr_users();
 ?>
