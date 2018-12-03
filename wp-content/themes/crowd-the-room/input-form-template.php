8e4b3e20bc8a88614f39ab4b77e3a70f22431975
dstroik24 <danielstroik@utexas.edu>
2018-12-03 14:19:53 -0600

    i losing patience

--- wp-content/themes/crowd-the-room/input-form-template.php
+++ wp-content/themes/crowd-the-room/input-form-template.php
@@ -21,9 +21,13 @@
     }
 
     </style>
-        <title>MAGA</title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+    <title>MAGA</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
+
+    <script src="/var/www/html/wp-content/themes/crowd-the-room/input-form.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqn3-tuxrnf_fQlyd4S3qmJWj4zvh1q10&libraries=places&callback=initAutocomplete"
+        async defer></script>
     </head>
 
     <body>
@@ -198,9 +202,6 @@
       </tr>
     </table>
 
-    <script src="/var/www/html/wp-content/themes/crowd-the-room/input-form.js"></script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAqn3-tuxrnf_fQlyd4S3qmJWj4zvh1q10&libraries=places&callback=initAutocomplete"
-        async defer></script>
 
 
 
