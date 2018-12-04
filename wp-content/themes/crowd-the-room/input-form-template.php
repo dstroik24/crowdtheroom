cc511f3820aa9865eb3ffa39b7cee88a3fe606bf
dstroik24 <danielstroik@utexas.edu>
2018-12-04 17:53:28 -0600

    update form styling

--- wp-content/themes/crowd-the-room/input-form-template.php
+++ wp-content/themes/crowd-the-room/input-form-template.php
@@ -11,6 +11,7 @@
     <head>
     <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
     <style>
+    <style>
     h1 { 
         font-family: Lato; 
         font-size: 23px; 
@@ -62,7 +63,166 @@
         font-weight: 400; 
         line-height: 23px; 
     }
+    
+    .form-style-8{
+        font-family: 'Lato';
+        width: 500px;
+        padding: 30px;
+        background: #FFFFFF;
+        margin: 50px auto;
+        box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+        -moz-box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.22);
+        -webkit-box-shadow:  0px 0px 15px rgba(0, 0, 0, 0.22);
 
+    }
+    .form-style-8 h2{
+        background: #4D4D4D;
+        text-transform: uppercase;
+        font-family: 'Lato';
+        color: #797979;
+        font-size: 18px;
+        font-weight: 100;
+        padding: 20px;
+        margin: -30px -30px 30px -30px;
+    }
+    .form-style-8 input[type="text"],
+    .form-style-8 input[type="date"],
+    .form-style-8 input[type="datetime"],
+    .form-style-8 input[type="email"],
+    .form-style-8 input[type="number"],
+    .form-style-8 input[type="search"],
+    .form-style-8 input[type="time"],
+    .form-style-8 input[type="url"],
+    .form-style-8 input[type="password"],
+    .form-style-8 textarea,
+    .form-style-8 select 
+    {
+        box-sizing: border-box;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        outline: none;
+        display: block;
+        width: 100%;
+        padding: 7px;
+        border: none;
+        border-bottom: 1px solid #ddd;
+        background: transparent;
+        margin-bottom: 10px;
+        font: 16px Lato;
+        height: 45px;
+    }
+    .form-style-8 textarea{
+        resize:none;
+        overflow: hidden;
+    }
+    .form-style-8 input[type="button"], 
+    .form-style-8 input[type="submit"]{
+        -moz-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+        -webkit-box-shadow: inset 0px 1px 0px 0px #45D6D6;
+        box-shadow: inset 0px 1px 0px 0px #45D6D6;
+        background-color: #2CBBBB;
+        border: 1px solid #27A0A0;
+        display: inline-block;
+        cursor: pointer;
+        color: #FFFFFF;
+        font-family: 'Lato';
+        font-size: 14px;
+        padding: 8px 18px;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+    .form-style-8 input[type="button"]:hover, 
+    .form-style-8 input[type="submit"]:hover {
+        background:linear-gradient(to bottom, #34CACA 5%, #30C9C9 100%);
+        background-color:#34CACA;
+    }
+
+
+
+    .form-style-4{
+        width: 450px;
+        font-size: 16px;
+        background: #91A8d0;
+        padding: 30px 30px 15px 30px;
+        border: 5px solid #ffffff;
+    }
+    .form-style-4 input[type=submit],
+    .form-style-4 input[type=button],
+    .form-style-4 input[type=text],
+    .form-style-4 input[type=email],
+    .form-style-4 textarea,
+    .form-style-4 label
+    {
+        font-family: Lato;
+        font-size: 16px;
+        color: #fff;
+        margin-bottom: 30px;
+
+    }
+    .form-style-4 select
+    {
+        font-family: Lato;
+        font-size: 16px;
+        margin-bottom: 30px;
+
+    }
+    .form-style-4 label {
+        display:block;
+        margin-bottom: 7px;
+    }
+    .form-style-4 label > span{
+        display: inline-block;
+        float: left;
+        width: 150px;
+    }
+    .form-style-4 input[type=text],
+    .form-style-4 input[type=email] 
+    {
+        background: transparent;
+        border: none;
+        border-bottom: 1px dashed #ffffff;
+        width: 275px;
+        outline: none;
+        padding: 0px 0px 0px 0px;
+        font-style: italic;
+    }
+    .form-style-4 textarea{
+        font-style: italic;
+        padding: 0px 0px 0px 0px;
+        background: transparent;
+        outline: none;
+        border: none;
+        border-bottom: 1px dashed #ffffff;
+        width: 275px;
+        overflow: hidden;
+        resize:none;
+        height:20px;
+    }
+
+    .form-style-4 textarea:focus, 
+    .form-style-4 input[type=text]:focus,
+    .form-style-4 input[type=email]:focus,
+    .form-style-4 input[type=email] :focus
+    {
+        border-bottom: 1px dashed #ffffff;
+    }
+
+    .form-style-4 input[type=submit],
+    .form-style-4 input[type=button]{
+        background: #576E86;
+        border: none;
+        padding: 8px 10px 8px 10px;
+        border-radius: 5px;
+        color: #A8BACE;
+    }
+    .form-style-4 input[type=submit]:hover,
+    .form-style-4 input[type=button]:hover{
+    background: #394D61;
+    }
+    #main-form {
+    width: 50%;
+    margin: 0 auto;
+    }
     </style>
 
     <title>MAGA</title>
@@ -105,8 +265,7 @@
             
             <div id="locationField">
                 <label for="autocomplete">Google maps autocomplete address:</label>
-                <input id="autocomplete" placeholder="Enter your address"
-                    onFocus="geolocate()" type="text" required></input>
+                <input id="autocomplete" onFocus="geolocate()" type="text" required></input>
             </div>
                 
             <div style="display:none;">
@@ -211,12 +370,14 @@
             dSelect.style.display = 'inline';
             dLabel.style.display = 'inline';
             if (officeVal == "us_rep"){
+                dSelect.add(new Option("Select your desired district", ""), null);
                 dSelect.add(new Option("10", "d10"), null);
                 dSelect.add(new Option("17", "d17"), null);
                 dSelect.add(new Option("21", "d21"), null);
                 dSelect.add(new Option("25", "d25"), null);
                 dSelect.add(new Option("35", "d35"), null);
             }else if (officeVal == "tx_rep"){
+                dSelect.add(new Option("Select your desired district", ""), null);
                 dSelect.add(new Option("46", "d46"), null);
                 dSelect.add(new Option("47", "d47"), null);
                 dSelect.add(new Option("48", "d48"), null);
@@ -224,6 +385,7 @@
                 dSelect.add(new Option("50", "d50"), null);
                 dSelect.add(new Option("51", "d51"), null);
             }else if (officeVal == "aisd"){
+                dSelect.add(new Option("Select your desired district", ""), null);
                 dSelect.add(new Option("1", "d1"), null);
                 dSelect.add(new Option("2", "d2"), null);
                 dSelect.add(new Option("3", "d3"), null);
