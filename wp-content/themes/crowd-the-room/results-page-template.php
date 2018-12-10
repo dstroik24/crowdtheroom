8246a1c968fabecda4177fa926b5ec53d28bf7e8
dstroik24 <danielstroik@utexas.edu>
2018-12-09 22:44:02 -0600

    debug time

--- wp-content/themes/crowd-the-room/results-page-template.php
+++ wp-content/themes/crowd-the-room/results-page-template.php
@@ -9,7 +9,7 @@ function get_user_info(){
 	$table_name = $wpdb->prefix . 'ctr_users';
     $sql = "SELECT * FROM " . $table_name . " WHERE user_id={$user_id}";
 	$result = $wpdb->get_row($sql, ARRAY_A);
-    //arr_as_table($result);
+    arr_as_table($result);
     return $result;
 }
 
